4a977c106145ac59c3db316b53c51473d38a9886
yuuki08noah <yuuki08noah@gmail.com>
2024-07-17 12:56:59 +0900

    DB

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import streamlit as st
 import json
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+from datetime import datetime
 
-# 메인 페이지
+with open("config/secret.json", "r") as f:
+    data = json.load(f)
 
+key = data['DBKey']
+cred = credentials.Certificate('config/firebase-key.json')
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred, {
+        'databaseURL': 'https://minor-piece-4ade8-default-rtdb.firebaseio.com/'
+    });
+
+# print(datetime.date)
+now = datetime.now()
+ref = db.reference(f'{now.date()}')
